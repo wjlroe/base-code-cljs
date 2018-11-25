0e28a82637cc1008ebb92d1970fb326720537707
William Roe <git@wjlr.org.uk>
2018-11-25 12:27:50 +0000

    Update project.clj a bit

--- project.clj
+++ project.clj
@@ -1,12 +1,35 @@
 (defproject base-code "1.0.0-SNAPSHOT"
-  :description "Base Code for my Ludum Dare #24 entry."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1450"]]
-  :plugins [[lein-cljsbuild "0.2.3"]]
+  :description "Base Code for Ludum Dare"
+  :url "https://github.com/wjlroe/base-code"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.7.1"
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
   :source-path "src/clj"
-  :cljsbuild {:builds [{
-                        :source-path "src/cljs"
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]}
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
+                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]
+                   :resource-paths ["target"]
+                   :clean-targets ^{:protect false} ["target"]}}
+  :cljsbuild {:builds [{:id "dev"
+                        :source-paths ["src/cljs"]
                         :compiler {
-                                   :output-to "resources/public/game.js"
-                                   :optimizations :whitespace
-                                   :pretty-print true}}]})
+                                   :main game.dev
+                                   :output-to "resources/public/js/compiled/out/game.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :optimizations :none
+                                   :cache-analysis true
+                                   :source-map true
+                                   :asset-path "js/compiled/out"}}
+                       {:id "release"
+                        :source-paths ["src/cljs"]
+                        :compiler {
+                                   :main game.main
+                                   :output-to "resources/public/js/compiled/out-adv/game.min.js"
+                                   :output-path "resource/public/js/compiled/out-adv"
+                                   :optimizations :advanced
+                                   :pretty-print false}}]})
